71952671fbc93d6e93a9818ef9d91eb9fe07ca79
6895mahfuzgit <6895mahfuz@gmail.com>
2021-08-08 18:15:52 +0600

    Create Torch Matrix and Vextor

---
+++ PyTorchTestApp.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  8 17:09:09 2021
+
+@author: Mahfuz_Shazol
+"""
+
+
+import torch
+x = torch.tensor(4.0)
+# print(x)
+# print(x.dtype)
+
+vectorTensor = torch.tensor([1,2,3,4.,5,6])
+# print(vectorTensor)
+# print(vectorTensor.dtype)
+
+
+matrixTensor = torch.tensor([
+                           [
+                            [1,2,3,4],
+                            [5,6,7,8]
+                            ],
+                           [
+                            [9,7,5,3],
+                            [1,2,3,7.]
+                            ]
+    ])
+# print(matrixTensor)
+# print(matrixTensor.dtype)
+# print(matrixTensor.shape)
+
+
+
+ 
+
+
+
+
+
+
+
+
